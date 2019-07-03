e4a9419fb3f74c9d81beff6a008755438072c5cf
Nicu Ciocan <prunku@gmail.com>
2019-07-03 14:16:29 +0300

    Support multiple resources

--- server.js
+++ server.js
@@ -4,19 +4,77 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const todos = [
-  {
-    id: 1,
-    text: 'Music is nice',
-  },
-  {
-    id: 2,
-    text: 'Look ma, no hands!',
-  },
-];
-let index = 0;
+const resources = {};
 
-const findTodoIndex = id => todos.findIndex(todo => todo.id === id);
+const findIndex = (collection, id) =>
+  collection.findIndex(item => item.id === id);
+
+const createResource = name => {
+  let id = 0;
+
+  resources[name] = [];
+
+  app.get('/' + name, (req, res) => {
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(resources[name]));
+  });
+
+  app.post('/' + name, (req, res) => {
+    const item = Object.assign(
+      {
+        id: ++id,
+      },
+      req.body
+    );
+
+    resources[name].push(item);
+
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(resources[name]));
+  });
+
+  app.get('/' + name + '/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = findIndex(id);
+
+    const item = resources[name][index];
+
+    if (!item) {
+      return res.send('Unknown index');
+    }
+    res.set('Content-Type', 'application/json');
+    res.send(item);
+  });
+
+  app.put('/' + name + '/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = findIndex(id);
+
+    if (!resources[name][index]) {
+      return res.send('Unknown index');
+    }
+
+    const item = Object.assign(resources[name][index], req.body);
+
+    resources[name][index] = todo;
+
+    res.set('Content-Type', 'application/json');
+    res.send(req.body.todo);
+  });
+
+  app.delete('/' + name + '/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = findIndex(id);
+    if (!resources[name][index]) {
+      return res.send('Unknown index');
+    }
+
+    resources[name].splice(index, 1);
+
+    res.set('Content-Type', 'application/json');
+    res.send(resources[name]);
+  });
+};
 
 app.use(cors());
 
@@ -25,65 +83,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/todos', (req, res) => {
-  res.set('Content-Type', 'application/json');
-  res.send(JSON.stringify(todos));
-});
-
-app.post('/todos', (req, res) => {
-  const todo = Object.assign(
-    {
-      id: ++index,
-    },
-    req.body
-  );
-
-  todos.push(todo);
-
-  res.set('Content-Type', 'application/json');
-  res.send(JSON.stringify(todos));
-});
-
-app.get('/todos/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = findTodoIndex(id);
-
-  const todo = todos[index];
-
-  if (!todo) {
-    return res.send('Unknown index');
-  }
-  res.set('Content-Type', 'application/json');
-  res.send(todo);
-});
-
-app.put('/todos/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = findTodoIndex(id);
-
-  if (!todos[index]) {
-    return res.send('Unknown index');
-  }
-
-  const todo = Object.assign(todos[index], req.body);
-
-  todos[index] = todo;
-
-  res.set('Content-Type', 'application/json');
-  res.send(req.body.todo);
-});
-
-app.delete('/todos/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = findTodoIndex(id);
-  if (!todos[index]) {
-    return res.send('Unknown index');
-  }
-
-  todos.splice(index, 1);
-
-  res.set('Content-Type', 'application/json');
-  res.send(todos);
-});
+createResource('todos');
+createResource('owners');
 
 app.listen(port, () => console.log(`Todos app listening on port ${port}!`));
